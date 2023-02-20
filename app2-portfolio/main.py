0e9cd1afb418464f79d254ff6086c877c66c44d6
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-02-20 18:22:15 +0300

    add description on the new columns

---
+++ app2-portfolio/main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Kendi Rita")
+    content = """
+            My name is Kendi and I'm a tech enthusiast, problem-solver, and software developer with a burning passion for all things programming.
+            I have been fortunate enough to spend a year honing my skills as a programmer at the prestigious ALX-Holberton school,
+            where I was able to work on exciting projects and develop my expertise in cutting-edge technologies. 
+            Whether it's building web applications or designing algorithms, 
+            I thrive on the challenge of creating innovative solutions that can make a real difference in people's lives.
+            
+        """
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in python.Feel free to contact me!
+"""
+st.write(content2)
